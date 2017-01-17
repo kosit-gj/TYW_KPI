b6fda70157937f3fdde6186d7184d663ffc8333e
GJ-Chokanan <chokanan@goingjesse.com>
2017-01-17 16:17:46 +0700

    Design Web Import Employee

---
+++ TYW_KPI/WebContent/Controller/import-employee.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	
+
+	$("#form_rule_item_all").change(function(){  //"select all" change 
+	    $(".from_data_rule").prop('checked', $(this).prop("checked")); //change all ".from_data_rule" checked status
+	});
+	//".from_data_rule" change 
+	$('.from_data_rule').change(function(){ 
+	    //uncheck "select all", if one of the listed from_data_rule item is unchecked
+	    if(false == $(this).prop("checked")){ //if this item is unchecked
+	        $("#form_rule_item_all").prop('checked', false); //change "select all" checked status to false
+	    }
+	    //check "select all" if all from_data_rule items are checked
+	    if ($('.from_data_rule:checked').length == $('.from_data_rule').length ){
+	        $("#form_rule_item_all").prop('checked', true);
+	    }
+	});
+	
+	
+});
