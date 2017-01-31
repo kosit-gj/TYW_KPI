0ae9eb71fee8c622d896d0750e4edd7115cb3aec
GJ-Chokanan <chokanan@goingjesse.com>
2017-01-31 11:22:10 +0700

    แก้ไข CSS & JS ที่ใช้ร่วมกันไปไว้ที่ tywMain.css

--- TYW_KPI/WebContent/Controller/common-data-set.js
+++ TYW_KPI/WebContent/Controller/common-data-set.js
@@ -1,54 +1,15 @@
 //Global variable
 var galbalDataCDS=[];
 //IP Server : 171.96.201.146
-var restfulURL="http://192.168.1.52";
+
 var restfulPathCDS="/tyw_api/public/cds";
+var restfulPathDropDownAppraisalLevel="/tyw_api/public/cds/al_list";
+var restfulPathDropDownConnection="/tyw_api/public/cds/connection_list";
+var restfulPathAutocomplete="/tyw_api/public/cds/auto_cds";
 
-var callFlashSlide = function(text,flashType){
-	if(flashType=="error"){
-		
-		$("#slide_status_area").html(text);
-		$("#slide_status").slideDown("slow");
-		
-	}else{
-		$("#slide_status_area").html(text);
-		$("#slide_status").slideDown("slow");
-		setTimeout(function(){
-			$("#slide_status").slideUp();
-		},8000);
-	}
-}
-var callFlashSlideInModal =function(text,id,flashType){
-	var btnClose="<div class=\"btnModalClose\">×</div>";
-	
-	if(flashType=="error"){
-		
-		if(id!=undefined){
-			$(id).html(btnClose+""+text).show();
-			
-		}else{
-			
-			$("#information").html(btnClose+""+text).show();
-		}
-		
-	}else{
-		
-		if(id!=undefined){
-			$(id).html(btnClose+""+text).show();
-		}else{
-			$("#information").html(btnClose+""+text).show();
-		}
-		setTimeout(function(){
-			if(id!=undefined){
-				$(id).hide("slow");
-			}else{
-				$("#information").hide("slow");
-			}
-		},8000);
-	}
-	
 
-}
+
+
 
 	//Check Validation
 var validationFn = function(data){
@@ -105,8 +66,7 @@ var getDataFn = function(page,rpp){
 		data:{"page":page,"rpp":rpp,
 			"cds_id":CdsName,
 			"appraisal_level_id":AppraisalLv},
-		//headers:{Authorization:"Bearer "+tokenID.token},
-		headers:{Authorization:"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
+		headers:{Authorization:"Bearer "+tokenID.token},
 		async:false,// w8 data 
 		success : function(data) {
 			
@@ -143,8 +103,7 @@ var findOneFn = function(id) {
 		url:restfulURL+restfulPathCDS+"/"+id,
 		type : "get",
 		dataType : "json",
-		//headers:{Authorization:"Bearer "+tokenID.token},
-		headers:{Authorization:"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
+		headers:{Authorization:"Bearer "+tokenID.token},
 		success : function(data) {		
 	
 			$("#f_cds_name").val(data['cds_name']);
@@ -260,7 +219,7 @@ var listCommonDataSetFn = function(data) {
 					 url:restfulURL+restfulPathCDS+"/"+id,
 					 type : "delete",
 					 dataType:"json",
-					 headers:{Authorization:"Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
+					 headers:{Authorization:"Bearer "+tokenID.token},
 					success:function(data){    
 				    	 
 					     if(data['status']==200){
@@ -317,7 +276,7 @@ var updateFn = function () {
 			"is_active":checkboxIsActive
 		},	
 		//headers:{Authorization:"Bearer "+tokenID.token},
-		headers:{Authorization:"Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
+		headers:{Authorization:"Bearer "+tokenID.token},
 		success : function(data) {
 			if (data['status'] == "200") {
 				getDataFn($("#pageNumber").val(),$("#rpp").val());
@@ -366,7 +325,7 @@ var insertFn = function (param) {
 			"is_active":checkboxIsActive
 		},	
 		//headers:{Authorization:"Bearer "+tokenID.token},
-		headers:{Authorization:"Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
+		headers:{Authorization:"Bearer "+tokenID.token},
 		success : function(data) {
 			
 			if (data['status'] == "200") {
@@ -410,11 +369,10 @@ var dropDownListAppraisalLevel = function(id,inputId){
 	html+="<select data-toggle=\"tooltip\" title=\"Appraisal Level\" class=\"input form-control input-sm\" id=\""+inputId+"\" name=\""+inputId+"\">";
 	//html+="<option selected value=''>All</option>";
 	$.ajax ({
-		//url:restfulURL+":3001/api/tyw_appraisal_level" ,
-		url:"http://192.168.1.52/tyw_api/public/cds/al_list",
+		url:restfulURL+restfulPathDropDownAppraisalLevel,
 		type:"get" ,
 		dataType:"json" ,
-		headers:{Authorization:"Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
+		headers:{Authorization:"Bearer "+tokenID.token},
 		async:false,
 		success:function(data){
 				
@@ -440,11 +398,11 @@ var dropDownListConnection = function(id){
 	html+="<select data-toggle=\"tooltip\" title=\"Connection\" class=\"input form-control input-sm\" id=\"f_connection\" name=\"f_connection\">";
 	//html+="<option  value=''>All</option>";
 	$.ajax ({
-		url:restfulURL+"/tyw_api/public/cds/connection_list" ,
+		url:restfulURL+restfulPathDropDownConnection ,
 		type:"get" ,
 		dataType:"json" ,
 		//headers:{Authorization:"Bearer "+tokenID.token},
-		headers:{Authorization:"Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
+		headers:{Authorization:"Bearer "+tokenID.token},
 		async:false,
 		success:function(data){
 				//galbalDqsRoleObj=data;
@@ -455,8 +413,6 @@ var dropDownListConnection = function(id){
 					html+="<option  value="+indexEntry["connection_id"]+">"+indexEntry["connection_name"]+"</option>";	
 				}
 			});	
-			
-
 		}
 	});	
 	html+="</select>";
@@ -464,81 +420,27 @@ var dropDownListConnection = function(id){
 };
 
 var executeFn = function (txtSQL) {
-	$.ajax ({
-		url:restfulURL+"..............." ,
-		type:"put" ,
-		dataType:"json" ,
-		//headers:{Authorization:"Bearer "+tokenID.token},
-		async:false,
-		success:function(data){
-				//galbalDqsRoleObj=data;
-//			if (data['status'] == "200") {
+	$.ajax({
+		url : restfulURL + "...............",
+		type : "put",
+		dataType : "json",
+		headers:{Authorization:"Bearer "+tokenID.token},
+		async : false,
+		success : function(data) {
+			// galbalDqsRoleObj=data;
+			if (data['status'] == "200") {
 				$("#txtSampleData").val(data['txtExecute']);
-				
-				}
-//		}else if (data['status'] == "400") {
-//			
-//			validationFn(data);
-//		}  
 
-		});	
-}
-//set paginate start
-var paginationSetUpFn = function(pageIndex,pageButton,pageTotal){
-	if(pageTotal==0){
-		pageTotal=1
-	}
-	$('.pagination_top,.pagination_bottom').off("page");
-	$('.pagination_top,.pagination_bottom').bootpag({
-	    total: pageTotal,//page Total
-	    page: pageIndex,//page index
-	    maxVisible: 5,//จำนวนปุ่ม
-	    leaps: true,
-	    firstLastUse: true,
-	    first: '←',
-	    last: '→',
-	    wrapClass: 'pagination',
-	    activeClass: 'active',
-	    disabledClass: 'disabled',
-	    nextClass: 'next',
-	    prevClass: 'prev',
-	    next: 'next',
-	    prev: 'prev',
-	    lastClass: 'last',
-	    firstClass: 'first'
-	}).on("page", function(event, num){
-		var rpp=10;
-		if($("#rpp").val()==undefined){
-			rpp=10;
-		}else{
-			rpp=$("#rpp").val();
+			} else if (data['status'] == "400") {
+
+				validationFn(data);
+			}
 		}
-		
-		getDataFn(num,rpp);
-		
-	    $(".pagingNumber").remove();
-	    var htmlPageNumber= "<input type='hidden' id='pageNumber' name='pageNumber' class='pagingNumber' value='"+num+"'>";
-	    $("body").append(htmlPageNumber);
-	   
-	}); 
-
-	$(".countPagination").off("change");
-	$(".countPagination").on("change",function(){
-
-		$("#countPaginationTop").val($(this).val());
-		$("#countPaginationBottom").val($(this).val());
-		
-		getDataFn(1,$(this).val());
-		
-		$(".rpp").remove();
-	    var htmlRrp= "<input type='hidden' id='rpp' name='rpp' class='rpp' value='"+$(this).val()+"'>";
-	    $("body").append(htmlRrp);
 	});
 }
-//set paginate end
+
 
 $(document).ready(function() {
-	//paginationSetUpFn(1,1,1);
 	
 	// ------------------- Common Data Set -------------------
 	
@@ -569,7 +471,6 @@ $(document).ready(function() {
 	
 	$("#btnSubmit").click(function(){
 		if ($("#action").val() == "add"|| $("#action").val() == "") {
-			//insertFn();
 			insertFn();
 		}else{
 			updateFn();
@@ -610,13 +511,10 @@ $(document).ready(function() {
 	$("#cds_name").autocomplete({
         source: function (request, response) {
         	$.ajax({
-				 url:"http://192.168.1.52/tyw_api/public/cds/auto_cds",
+				 url:restfulURL+restfulPathAutocomplete,
 				 type:"post",
 				 dataType:"json",
-				 headers:{Authorization:"Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cLzE3MS45Ni4yMDAuMjBcL3R5d19hcGlcL3B1YmxpY1wvc2Vzc2lvbiIsImlhdCI6MTQ4NTc0MjU0NSwiZXhwIjoxNDg1Nzc4NTQ1LCJuYmYiOjE0ODU3NDI1NDUsImp0aSI6IjFmYmU0NWFmNTYwMTNmMDEwMDQ4NzM2MGU1N2M4MzM2In0.Yq4nRTacwg14de3Fg2QVyztrXkyiqFLROzCBwXMN9Po"},
-//tokenID.token
-//appraisal_level_id,
-//cds_name
+				 headers:{Authorization:"Bearer "+tokenID.token},
 				 data:{"appraisal_level_id":$("#app_lv").val(),"cds_name":request.term},
 				 //async:false,
                  error: function (xhr, textStatus, errorThrown) {
