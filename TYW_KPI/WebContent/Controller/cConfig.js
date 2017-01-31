de960ef4792092ef6b77bf700fe05f0c5b81ad0d
GJ-Chokanan <chokanan@goingjesse.com>
2017-01-31 09:47:41 +0700

    Edit Main Js

--- TYW_KPI/WebContent/Controller/cConfig.js
+++ TYW_KPI/WebContent/Controller/cConfig.js
@@ -1,2 +1,2 @@
 //restful URL
-var restfulURL="http://192.168.1.58";
+var restfulURL="http://192.168.1.52";
--- TYW_KPI/WebContent/Controller/cMain.js
+++ TYW_KPI/WebContent/Controller/cMain.js
@@ -1,5 +1,105 @@
 var tokenID= eval("("+localStorage.getItem("tokenID")+")");
 
+var callFlashSlide = function(text,flashType){
+	if(flashType=="error"){
+		
+		$("#slide_status_area").html(text);
+		$("#slide_status").slideDown("slow");
+		
+	}else{
+		$("#slide_status_area").html(text);
+		$("#slide_status").slideDown("slow");
+		setTimeout(function(){
+			$("#slide_status").slideUp();
+		},8000);
+	}
+}
+var callFlashSlideInModal =function(text,id,flashType){
+	var btnClose="<div class=\"btnModalClose\">×</div>";
+	
+	if(flashType=="error"){
+		
+		if(id!=undefined){
+			$(id).html(btnClose+""+text).show();
+			
+		}else{
+			
+			$("#information").html(btnClose+""+text).show();
+		}
+		
+	}else{
+		
+		if(id!=undefined){
+			$(id).html(btnClose+""+text).show();
+		}else{
+			$("#information").html(btnClose+""+text).show();
+		}
+		setTimeout(function(){
+			if(id!=undefined){
+				$(id).hide("slow");
+			}else{
+				$("#information").hide("slow");
+			}
+		},8000);
+	}
+	
+
+}
+
+//set paginate start
+var paginationSetUpFn = function(pageIndex,pageButton,pageTotal){
+	if(pageTotal==0){
+		pageTotal=1
+	}
+	$('.pagination_top,.pagination_bottom').off("page");
+	$('.pagination_top,.pagination_bottom').bootpag({
+	    total: pageTotal,//page Total
+	    page: pageIndex,//page index
+	    maxVisible: 5,//จำนวนปุ่ม
+	    leaps: true,
+	    firstLastUse: true,
+	    first: '←',
+	    last: '→',
+	    wrapClass: 'pagination',
+	    activeClass: 'active',
+	    disabledClass: 'disabled',
+	    nextClass: 'next',
+	    prevClass: 'prev',
+	    next: 'next',
+	    prev: 'prev',
+	    lastClass: 'last',
+	    firstClass: 'first'
+	}).on("page", function(event, num){
+		var rpp=10;
+		if($("#rpp").val()==undefined){
+			rpp=10;
+		}else{
+			rpp=$("#rpp").val();
+		}
+		
+		getDataFn(num,rpp);
+		
+	    $(".pagingNumber").remove();
+	    var htmlPageNumber= "<input type='hidden' id='pageNumber' name='pageNumber' class='pagingNumber' value='"+num+"'>";
+	    $("body").append(htmlPageNumber);
+	   
+	}); 
+
+	$(".countPagination").off("change");
+	$(".countPagination").on("change",function(){
+
+		$("#countPaginationTop").val($(this).val());
+		$("#countPaginationBottom").val($(this).val());
+		
+		getDataFn(1,$(this).val());
+		
+		$(".rpp").remove();
+	    var htmlRrp= "<input type='hidden' id='rpp' name='rpp' class='rpp' value='"+$(this).val()+"'>";
+	    $("body").append(htmlRrp);
+	});
+}
+//set paginate end
+
 
 
 
